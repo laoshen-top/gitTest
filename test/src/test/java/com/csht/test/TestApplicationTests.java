a6258796583f8172734ad05cf687a9967c503b54
shenzening <1252307560@qq.com>
2026-01-13 16:10:52 +0800

    init

---
+++ test/src/test/java/com/csht/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csht.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
